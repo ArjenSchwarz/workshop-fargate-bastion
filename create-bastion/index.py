901eaf042d507c37de4051d51f14cac99466db36
Arjen Schwarz <arjen.schwarz@bulletproof.net>
2018-07-10 22:17:48 +1000

    Final code
    * Improved create-bastion functionality
    * Delete single bastion function and trigger
    * Delete all bastions function

--- create-bastion/index.py
+++ create-bastion/index.py
@@ -14,6 +14,18 @@ subnet_string = os.environ['BASTION_SUBNETS']
 subnet_array = subnet_string.split(',')
 vpc = os.environ['BASTION_VPC']
 
+def ipResponse(ip):
+    response = {}
+    response['statusCode'] = 200
+    response['body'] = ip
+    return response
+
+def failResponse(error):
+    response = {}
+    response['statusCode'] = 500
+    response['body'] = error
+    return response
+
 def lambda_handler(event, context):
     user = event['queryStringParameters']['user']
     ip = event['requestContext']['identity']['sourceIp'] + "/32"
@@ -21,6 +33,44 @@ def lambda_handler(event, context):
     ecs = boto3.client('ecs')
 
     bastion_name = 'bastion-' + user
+
+    try:
+        # Check if everything already exists, if so return that
+        ec2.describe_security_groups(
+            Filters=[
+                {'Name': 'vpc-id', 'Values': [vpc]},
+                {'Name': 'group-name', 'Values': [bastion_name]}
+            ]
+        )
+        # TODO: check that the IPs still match, otherwise switch them out
+        running_tasks = ecs.list_tasks(
+            cluster=bastion_cluster,
+            family=bastion_name,
+            desiredStatus='RUNNING'
+        )
+        # TODO: if securitygroup found, but no task delete the securitygroup before proceeding
+        if len(running_tasks['taskArns']) > 0:
+            tasklist = ecs.describe_tasks(cluster=bastion_cluster, tasks=running_tasks['taskArns'])
+            task_arn = tasklist['tasks'][0]['taskArn']
+            attachment_id = tasklist['tasks'][0]['attachments'][0]['id']
+            attachment_identifier = "attachment/" + attachment_id
+            attachment_description = re.sub(r'task/.*', attachment_identifier, task_arn)
+            eni_description = ec2.describe_network_interfaces(
+                Filters=[
+                    {
+                        'Name': 'description',
+                        'Values': [attachment_description]
+                    }
+                ]
+            )
+            ip = eni_description['NetworkInterfaces'][0]['Association']['PublicIp']
+
+            return ipResponse(ip)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'InvalidGroup.NotFound':
+            print("SecurityGroup doesn't exist yet")
+        else:
+            failResponse(e.response)
 
     # Create the security group
     sg_response = ec2.create_security_group(
@@ -55,8 +105,32 @@ def lambda_handler(event, context):
             }
         }
     )
+    task_arn = response['tasks'][0]['taskArn']
+    attachment_id = response['tasks'][0]['attachments'][0]['id']
+    attachment_identifier = "attachment/" + attachment_id
+    attachment_description = re.sub(
+        r'task/.*', attachment_identifier, task_arn)
 
-    response = {}
-    response['statusCode'] = 200
-    response['body'] = 'Hello ' + user + " from " + ip
-    return response
+    # It takes a bit of time to get the ENI, check after a couple of seconds and then loop
+    time.sleep(2)
+    eni_description = ec2.describe_network_interfaces(
+        Filters=[
+            {
+                'Name': 'description',
+                'Values': [attachment_description]
+            }
+        ]
+    )
+    while len(eni_description['NetworkInterfaces']) == 0:
+        time.sleep(2)
+        eni_description = ec2.describe_network_interfaces(
+            Filters=[
+                {
+                    'Name': 'description',
+                    'Values': [attachment_description]
+                }
+            ]
+        )
+    ip = eni_description['NetworkInterfaces'][0]['Association']['PublicIp']
+
+    return ipResponse(ip)
